c468ee8364e8c3507006c7d4b25569031547e992
Anthony Damico <ajdamico@gmail.com>
2017-08-04 07:11:32 -0400

    dhs fix

--- tests/setup.R
+++ tests/setup.R
@@ -7,9 +7,9 @@ library(lodown)
 dhs_cat <-
 	get_catalog( "dhs" ,
 		output_dir = file.path( getwd() ) , 
-		your_email = "my_email_address" , 
-		your_password = "my_password" ,
-		your_project = "my_project" )
+		your_email = my_email_address , 
+		your_password = my_password ,
+		your_project = my_project )
 
 # sample 10% of the records
 which_records <- sample( seq( nrow( dhs_cat ) ) , round( nrow( dhs_cat ) * 0.10 ) )
@@ -18,7 +18,7 @@ which_records <- sample( seq( nrow( dhs_cat ) ) , round( nrow( dhs_cat ) * 0.10 
 dhs_cat <- unique( rbind( dhs_cat[ which_records , ] , subset( dhs_cat , year == 2004 & country == 'Malawi' & grepl( "MWIR4DDT" , full_url ) ) ) )
 
 lodown( "dhs" , dhs_cat , 
-		your_email = "my_email_address" , 
-		your_password = "my_password" ,
-		your_project = "my_project" )
+		your_email = my_email_address , 
+		your_password = my_password ,
+		your_project = my_project )
 
